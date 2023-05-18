87a9080e607c985c1368dcef2b634ff9adb3c3c5
kelvinjou <63611619+kelvinjou@users.noreply.github.com>
2023-05-17 19:09:20 -0700

    Initial Commit

---
+++ VirtualShowrooms/VirtualShowroomsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VirtualShowroomsApp.swift
+//  VirtualShowrooms
+//
+//  Created by Kelvin J on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct VirtualShowroomsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
